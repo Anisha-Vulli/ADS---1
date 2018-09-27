9b464bb6cb962f3be27b930351ed84a5d8d7f79d
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-09-27 19:44:54 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS - 1 assignments/M 4/4.1 Balanced Paranthesis/UsingLL.java
+++ ADS - 1 assignments/M 4/4.1 Balanced Paranthesis/UsingLL.java
@@ -1,20 +1,38 @@
 import java.util.Scanner;
-import java.util.Arrays;
+//import java.util.Arrays;
+
+/**
+ * Class for node.
+ */
 class Node {
 	String data;
 	Node next;
 }
 
+/**
+ * Class for linkedlist.
+ */
 class Linkedlist {
+	/**
+	 * An empty node.
+	 */
 	private Node start = null;
-
+	/**
+	 * Pushing an element into list.
+	 *
+	 * @param      ch    { given String }
+	 */
 	void push(final String ch) {
 		Node val = start;
 		start = new Node();
 		start.data = ch;
 		start.next = val;
 	}
-
+	/**
+	 * Popping element from list.
+	 *
+	 * @return     { String value }
+	 */
 	String pop() {
 		if (start == null) {
             return "e";
@@ -23,16 +41,33 @@ class Linkedlist {
         start = start.next;
         return r;
 	}
-
+	/**
+	 * First value.
+	 *
+	 * @return     { value }
+	 */
 	public Node gethead() {
         return start;
     }
 }
+/**
+ * Class for balanced.
+ */
 class Balanced {
+	/**
+	 * Constructs the object.
+	 */
     Balanced() {
 
     }
-
+    /**
+     * Checking if the pair is matched or not.
+     *
+     * @param      str1  The string 1
+     * @param      str2  The string 2
+     *
+     * @return     { a boolean }
+     */
     boolean Matchingpair (String str1, String str2) {
         if (str1.equals("(") && str2.equals(")")) {
             return true;
@@ -45,7 +80,13 @@ class Balanced {
         }
         return false;
     }
-
+    /**
+     * Balancing of parnathesis.
+     *
+     * @param      array  The array
+     *
+     * @return     { boolean based on matching }
+     */
     boolean balancing(final String[] array) {
         Linkedlist ll = new Linkedlist();
         for (int i = 0; i < array.length; i++) {
@@ -63,8 +104,16 @@ class Balanced {
         return ll.gethead() == null;
     }
 }
+/**
+ * Class for using ll.
+ */
 class UsingLL {
-	public static void main(String[] args) {
+	/**
+	 * Main method.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = Integer.parseInt(sc.nextLine());
         while(n != 0) {
