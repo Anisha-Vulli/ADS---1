ac481961cecbb79488945c19a412a002f082975e
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-09-27 21:10:09 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ ADS - 1 practice/M 4/A 6/4.2 Evaluate Postfix/Solution.java
@@ -0,0 +1,89 @@
+/**
+ * { Importing Scannner }.
+ */
+import java.util.Scanner;
+/**
+ * { Importing Arrays }.
+ */
+import java.util.Arrays;
+/**
+ * Class for node.
+ */
+class Node {
+    /**
+     * Data in string.
+     */
+    int data;
+    /**
+     * Next node.
+     */
+    Node next;
+}
+
+/**
+ * Class for linkedlist.
+ */
+class Linkedlist {
+    /**
+     * An empty node.
+     */
+    private Node start = new Node();
+    /**
+     * Pushing an element into list.
+     *
+     * @param      ch    { given String }
+     */
+    void push(final int value) {
+        Node val = start;
+        start = new Node();
+        start.data = value;
+        start.next = val;
+    }
+    /**
+     * Popping element from list.
+     *
+     * @return     { String value }
+     */
+    int pop() {
+        int c = start.data;
+        start = start.next;
+        return c;
+    }
+    /**
+     * First value.
+     *
+     * @return     { value }
+     */
+    public boolean isEmpty() {
+        return (start.next == null);
+    }
+}
+
+class Solution {
+    public static void main(String[] args) {
+        Linkedlist ll = new Linkedlist();
+        int popelement1 = 0;
+        int popelement2 = 0;
+        Scanner sc = new Scanner(System.in);
+        String[] input = sc.nextLine().split(",");
+        for (int i = 0; i < input.length; i++) {
+           if (!(Arrays.asList("+", "-", "*", "/").contains(input[i]))) {
+                ll.push(Integer.parseInt(input[i]));
+            } else {
+                popelement1 = ll.pop();
+                popelement2 = ll.pop();
+                if (input[i].equals("+")) {
+                    ll.push(popelement1 + popelement2);
+                } else if (input[i].equals("-")) {
+                    ll.push(popelement1 - popelement2);
+                } else if (input[i].equals("*")) {
+                    ll.push(popelement1 * popelement2);
+                } else {
+                    ll.push(popelement1 / popelement2);
+                }
+            }
+        }
+        System.out.println(ll.pop());
+    }
+}
+
