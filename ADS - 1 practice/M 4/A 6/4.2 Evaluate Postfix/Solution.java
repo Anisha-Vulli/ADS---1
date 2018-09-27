f5f71dab630ba3957a2b48b4efd629eb89e8ba39
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-09-27 21:12:44 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS - 1 practice/M 4/A 6/4.2 Evaluate Postfix/Solution.java
+++ ADS - 1 practice/M 4/A 6/4.2 Evaluate Postfix/Solution.java
@@ -7,58 +7,8 @@ import java.util.Scanner;
  */
 import java.util.Arrays;
 /**
- * Class for node.
+ * Class for solution.
  */
-class Node {
-    /**
-     * Data in string.
-     */
-    int data;
-    /**
-     * Next node.
-     */
-    Node next;
-}
-
-/**
- * Class for linkedlist.
- */
-class Linkedlist {
-    /**
-     * An empty node.
-     */
-    private Node start = new Node();
-    /**
-     * Pushing an element into list.
-     *
-     * @param      value  The value
-     */
-    void push(final int value) {
-        Node val = start;
-        start = new Node();
-        start.data = value;
-        start.next = val;
-    }
-    /**
-     * Popping element from list.
-     *
-     * @return     { int value }
-     */
-    int pop() {
-        int c = start.data;
-        start = start.next;
-        return c;
-    }
-    /**
-     * First value.
-     *
-     * @return     { value }
-     */
-    public boolean isEmpty() {
-        return (start.next == null);
-    }
-}
-
 class Solution {
     public static void main(final String[] args) {
         Linkedlist ll = new Linkedlist();
