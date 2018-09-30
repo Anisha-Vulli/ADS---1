cc8598a3a2c7a6ac6f1ad812f4be79fd9037d2d8
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-09-30 14:36:02 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- ADS - 1 assignments/M 5/5.2 Josephus Problem/Solution.java
+++ ADS - 1 assignments/M 5/5.2 Josephus Problem/Solution.java
@@ -1,28 +1,35 @@
 import java.util.Scanner;
-import java.util.Arrays;
+//import java.util.Arrays;
 
+final public class Solution {
+    Solution() {
+        //Empty constructor.
+    }
+    /**
+     * Main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = Integer.parseInt(sc.nextLine());
+        while (sc.hasNext()) {
+            String[] input = sc.nextLine().split(" ");
+            int m = Integer.parseInt(input[0]);
+            int n = Integer.parseInt(input[1]);
+            Queue q = new Queue();
+            String str = "";
+            for (int i = 0; i < m; i++) {
+                q.enqueue(i);
+            }
 
-class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int num = Integer.parseInt(sc.nextLine());
-		while (sc.hasNext()) {
-			String[] input = sc.nextLine().split(" ");
-			int m = Integer.parseInt(input[0]);
-			int n = Integer.parseInt(input[1]);
-			Queue q = new Queue();
-			String str = "";
-			for (int i = 0; i < m; i++) {
-				q.enqueue(i);
-			}
-
-			while (!q.isEmpty()) {
+            while (!q.isEmpty()) {
                 for (int k = 0; k < n - 1; k++) {
                     q.enqueue(q.dequeue());
                 }
                 str += q.dequeue() + " ";
             }
             System.out.println(str.substring(0, str.length() - 1));
-		}
-	}
+        }
+    }
 }
