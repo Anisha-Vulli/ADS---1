7d5b8a515d40b18425afda18732fd3ebd3b89cbe
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-10-01 12:03:02 +0530

    "check_style_score -> 1 of 2 passed. style score: 0/1 "

--- ADS - 1 assignments/M 7/7.1 Steque/Solution.java
+++ ADS - 1 assignments/M 7/7.1 Steque/Solution.java
@@ -110,12 +110,12 @@ class Solution {
                 case "pop":
                 if (!sq.isEmpty()) {
                     sq.pop();
-                    System.out.println(sq);
-                    // if (!sq.isEmpty()) {
-                    //     System.out.println(sq);    
-                    // } else {
-                    //     System.out.println("Steque is empty.");
-                    // }  
+                    //System.out.println(sq);
+                    if (!sq.isEmpty()) {
+                        System.out.println(sq);    
+                    } else {
+                        System.out.println("Steque is empty.");
+                    }  
                 } else {
                     System.out.println("Steque is empty.");
                 }
