cb20e2a4b2db61cc7f5911a391a8d0a1692c0661
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-10-02 18:46:53 +0530

    "check_style_score -> 6 of 6 passed. style score: 1/1 "

--- ADS - 1 assignments/M 8/8.1 Team Ranking With Insertion/Solution.java
+++ ADS - 1 assignments/M 8/8.1 Team Ranking With Insertion/Solution.java
@@ -1,8 +1,24 @@
+/**
+ * Scanner method.
+ */
 import java.util.Scanner;
-
-
+/**
+ * Class for solution.
+ */
 class Solution {
-    
+    /**
+     * Constructs the object.
+     */
+    protected Solution() {
+        //Empty constructor.
+    }
+    /**
+     * Creates a team.
+     *
+     * @param      array  The array
+     *
+     * @return     { description_of_the_return_value }
+     */
     public static Team createTeam(final String[] array) {
         String name = array[0];
         //System.out.println(name);
@@ -11,7 +27,12 @@ class Solution {
         int draws = Integer.parseInt(array[2 + 1]);
         return new Team(name, wins, losses, draws);
     }
-    public static void main(String[] args) {
+    /**
+     * Main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         Sort s = new Sort();
         while (sc.hasNextLine()) {
