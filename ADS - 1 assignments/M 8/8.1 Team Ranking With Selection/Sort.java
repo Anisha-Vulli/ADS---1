4be02d8c1f0cdd7dfe3bb41776271dd82b4fe1c5
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-10-02 19:51:55 +0530

    "check_style_score -> 6 of 6 passed. style score: 1/1 "

--- ADS - 1 assignments/M 8/8.1 Team Ranking With Selection/Sort.java
+++ ADS - 1 assignments/M 8/8.1 Team Ranking With Selection/Sort.java
@@ -1,11 +1,28 @@
 import java.util.Arrays;
+/**
+ * Class for sort.
+ */
 class Sort{
+	/**
+	 * Teamarray with team objects.
+	 */
 	public Team[] teamarray = new Team[10];
+	/**
+	 * Teamcount for increasing count in array.
+	 */
 	int teamcount;
+	/**
+	 * Constructs the object.
+	 */
  	Sort() {
  		teamcount = 0;
 	}
-
+	/**
+	 * Adds a team.
+	 * Time complexity N. It depends on the number of values added into array.
+	 *
+	 * @param      teamgiven  The teamgiven
+	 */
 	void addTeam(Team teamgiven) {
 		try {
 			teamarray[teamcount++] = teamgiven;
@@ -15,17 +32,32 @@ class Sort{
 		}
 		teamarray[teamcount++] = teamgiven;
 	}
-
+	/**
+	 * Resizes the array and makes a copy of the array.
+	 */
 	void resize() {
 		teamarray = Arrays.copyOf(teamarray, 2 * teamcount);
 	}
 
+	/**
+	 * Swaps the values and places the smaller one in the beginning.
+	 * Time complexity is 1 only assigning of values.
+	 *
+	 * @param      teams  The teams
+	 * @param      i      { Index value. }
+	 * @param      j      { Index value. }
+	 */
 	void exchange(Team[] teams, int i, int j) {
 		Team t = teams[j];
 		teams[j] = teams[i];
 		teams[i] = t;
 	}
 
+	/**
+	 * Returns a string representation of the object.
+	 *
+	 * @return     String representation of the object.
+	 */
 	public String toString() {
 		String str = "";
 		for (int i = 0; i< teamcount; i++) {
@@ -33,6 +65,16 @@ class Sort{
 		}
 		return str.substring(0, str.length() - 1);
 	}
+
+	/**
+	 * Selection sort.
+	 * 
+	 * The time complexity of this technique is N^2.
+	 * For each and every iteration the value is taken
+	 * The object is compared to the adjecent one checking if the value is 
+	 * lesser or not if less the exchange is happening.
+	 * 
+	 */
 	void Selectionsort() {
 		
 		for (int i = 0; i < teamcount-1; i++) {
@@ -47,6 +89,15 @@ class Sort{
 		//System.out.println(toString());
 	}
 
+	/**
+	 * Less comparision.
+	 *
+	 * @param      arr   The arr
+	 * @param      i     { Index value. }
+	 * @param      j     { Index value. }
+	 *
+	 * @return     { Boolean value based on the comparision }
+	 */
 	boolean less(Team[] arr, int i , int j){
 
 		return arr[i].compareTo(arr[j]) > 0;
--- ADS - 1 assignments/M 8/8.1 Team Ranking With Selection/Team.java
+++ ADS - 1 assignments/M 8/8.1 Team Ranking With Selection/Team.java
@@ -1,8 +1,22 @@
+/**
+ * Class for team.
+ */
 class Team implements Comparable<Team> {
+	/**
+	 * The variables read are name, wins, losses, and draws.
+	 */
 	String name;
 	int wins;
 	int losses;
 	int draws;
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      name1    The name 1
+	 * @param      wins1    The wins 1
+	 * @param      losses1  The losses 1
+	 * @param      draws1   The draws 1
+	 */
 	Team (String name1, int wins1, int losses1, int draws1) {
 		this.name = name1;
 		this.wins = wins1;
@@ -26,6 +40,19 @@ class Team implements Comparable<Team> {
 		return draws;
 	}
 
+	/**
+	 * Compare two objects and returns values related to it.
+	 * If one is greater than other it return 1 else returns -1
+	 * and 0 if both equals.
+	 * 
+	 * The time complexity is only 1 here only condition is
+	 * getting verified and based on the verification the value is
+	 * getting returned.
+	 *
+	 * @param      obj   The object
+	 *
+	 * @return     { integer value 0 or 1 or -1 }
+	 */
 	public int compareTo(Team obj) {
 		if (this.wins > obj.wins) {
 			return 1;
