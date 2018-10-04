d8bc9767f56384cedc2b0caf89df9328e41cd04e
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-10-04 12:20:10 +0530

    a8 commit

--- ADS - 1 practice/M 9/A 8/Solution.java
+++ ADS - 1 practice/M 9/A 8/Solution.java
@@ -1,11 +1,27 @@
 import java.util.Scanner;
+/**
+ * Class for solution.
+ */
 class Solution {
+	/**
+	 * Main function.
+	 *
+	 * @param      args  The arguments
+	 */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		System.out.println(fact(n));
 	}
 
+	/**
+	 * Factorial caluculation function.
+	 * Here factorial is calculated in recursion so the complexity is N.
+	 *
+	 * @param      n     { Number sent }
+	 *
+	 * @return     { factorial of specified number }
+	 */
 	public static int fact(int n) {
 		if (n == 0) {
 			return 1;
