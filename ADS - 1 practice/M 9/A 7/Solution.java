ef4ccb9a5e8bb605a71fd49a737f7adda8acdd16
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-10-04 12:19:40 +0530

    a7 commit

--- ADS - 1 practice/M 9/A 7/Solution.java
+++ ADS - 1 practice/M 9/A 7/Solution.java
@@ -1,11 +1,29 @@
 import java.util.Scanner;
+/**
+ * Class for solution.
+ */
 class Solution {
+	/**
+	 * Main function.
+	 *
+	 * @param      args  The arguments
+	 */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String input = sc.nextLine();
 		System.out.println(stars(input));
 	}
 
+	/**
+	 * Stars appending in between the letters.
+	 * The function operates throughout the length of the string if the
+	 * same letters are found at the end of the string.
+	 * Time complexity of this O(N).
+	 *
+	 * @param      str   The string
+	 *
+	 * @return     { String after appending stars }
+	 */
 	public static String stars(String str) {
 		if (str.length() == 0 || str.length() == 1) {
 			return str;
