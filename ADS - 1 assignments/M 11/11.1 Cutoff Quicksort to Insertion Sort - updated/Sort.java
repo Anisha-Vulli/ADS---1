2d6970dde3a1f0ee991ca68c3af89b8bf776992c
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-10-07 15:28:55 +0530

    "ads1_m11_a1_v2: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS - 1 assignments/M 11/11.1 Cutoff Quicksort to Insertion Sort - updated/Sort.java
+++ ADS - 1 assignments/M 11/11.1 Cutoff Quicksort to Insertion Sort - updated/Sort.java
@@ -1,7 +1,26 @@
+/**
+ * Class for sort.
+ */
 class Sort {
+	/**
+	 * Constructs the object.
+	 */
 	Sort() {
 	}
 
+	/**
+	 * Partition function.
+	 * This is used to partion the array into two halves.
+	 * The time complexity of the function is N^2.
+	 * In the worst case the while loop executes N times.
+	 * There are two while loops and each take N times of execution.
+	 *
+	 * @param      array  The array
+	 * @param      low    The low
+	 * @param      high   The high
+	 *
+	 * @return     { value. }
+	 */
 	public int partition(Comparable[] array, int low, int high) {
 		int i = low;
 		int j = high + 1;
@@ -30,6 +49,16 @@ class Sort {
 		return j;
 	}
 
+	/**
+	 * Quick sort.
+	 * If number of values in the array are less than 8.
+	 * The complexity of the function is 1.
+	 *
+	 * @param      array   The array
+	 * @param      low     The low
+	 * @param      high    The high
+	 * @param      cutoff  The cutoff
+	 */
 
 	public void quickSort(Comparable[] array, int low, int high, int cutoff) {
 		if (high <= low + cutoff - 1) {
@@ -43,10 +72,27 @@ class Sort {
 		quickSort(array, j + 1, high, cutoff);
 	}
 
+	/**
+	 * The array and the cutoff value sent from the
+	 * main function are sent to the quick sort function.
+	 * The complexity  of the function is 1.
+	 *
+	 * @param      array   The array
+	 * @param      cutoff  The cutoff
+	 */
 	public void quickSort(Comparable[] array, int cutoff) {
 		quickSort(array, 0, array.length - 1, cutoff);
 	}
 
+	/**
+	 * Insertion sort function.
+	 * Here the given array is sorted based on the insertion sort.
+	 * Complexity of insertion sort is N^2.
+	 *
+	 * @param      array  The array
+	 * @param      low    The low
+	 * @param      high   The high
+	 */
 	public void insertionSort(Comparable[] array, int low, int high) {
 		for (int i = low; i <= high; i++) {
 			for (int j = i; j > low && less(array[j], array[j - 1]); j--) {
@@ -55,16 +101,41 @@ class Sort {
 		}
 	}
 
+	/**
+	 * Swapping function.
+	 * The values sent from the insertion sort are sent to
+	 * swap with each other.
+	 * Complexity is 1.
+	 *
+	 * @param      array  The array
+	 * @param      i      { parameter_description }
+	 * @param      j      { parameter_description }
+	 */
 	public void exchange(Comparable[] array, int i, int j) {
 		Comparable swap = array[i];
 		array[i] = array[j];
 		array[j] = swap;
 	}
 
+	/**
+	 * Less function compares the two strings.
+	 *
+	 * @param      a     { parameter_description }
+	 * @param      b     { parameter_description }
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	public boolean less(Comparable a, Comparable b) {
 		return a.compareTo(b) < 0;
 	}
 
+	/**
+	 * Returns a string representation of the object.
+	 *
+	 * @param      a     { parameter_description }
+	 *
+	 * @return     String representation of the object.
+	 */
 	public Object toString(Object[] a) {
 		String s = "[";
 		int i;
