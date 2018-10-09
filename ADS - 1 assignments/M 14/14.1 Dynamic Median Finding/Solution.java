514371ce8b5aee25a06a3610caf26decd7b7b72a
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-10-09 22:41:23 +0530

    "ads1_m14_a1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS - 1 assignments/M 14/14.1 Dynamic Median Finding/Solution.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+class Solution {
+	Solution() {
+    }
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+        MinPQ<Float> minheap = new MinPQ<Float>();
+        MaxPQ<Float> maxheap = new MaxPQ<Float>();
+        Float median = 0.0f;
+    	int lines = sc.nextInt();
+        for (int i = 0; i < lines; i++) {
+        	Float val = sc.nextFloat();
+            if (val > median) {
+                minheap.insert(val);
+            } else {
+                maxheap.insert(val);
+            }
+
+            if (minheap.size() - maxheap.size() > 1) {
+                maxheap.insert(minheap.delMin());
+            }
+
+            if (maxheap.size() - minheap.size() > 1) {
+                minheap.insert(maxheap.delMax());
+            }
+
+            if (minheap.size() == maxheap.size()) {
+                median = (minheap.min() + maxheap.max()) / 2;
+                System.out.println(median);
+            }
+
+            if (maxheap.size() > minheap.size()) {
+                median = maxheap.max();
+                System.out.println(median);
+            }
+        	
+            if (minheap.size() > maxheap.size()) {
+                median = minheap.min();
+                System.out.println(median);
+            }
+        }
+    }
+}
