16dd61d093054c33544383fb31b23a3c1e42a001
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-10-09 23:51:22 +0530

    "ads1_m14_a2: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS - 1 assignments/M 14/14.2 student reservation system/Student.java
+++ ADS - 1 assignments/M 14/14.2 student reservation system/Student.java
@@ -1,11 +1,47 @@
+/**
+ * Class for student.
+ */
 class Student implements Comparable<Student> {
+    /**
+     * Name of the student.
+     */
     private String name;
+    /**
+     * Date of birth of student.
+     */
     private String dob;
+    /**
+     * Marks1 of student.
+     */
     private int marks1;
+    /**
+     * Marks2 of student.
+     */
     private int marks2;
+    /**
+     * Marks3 of student.
+     */
     private int marks3;
+    /**
+     * Total of student.
+     */
     private int total;
+    /**
+     * Category of student.
+     */
     private String cat;
+
+    /**
+     * Constructs the object.
+     *
+     * @param      name1    The name 1
+     * @param      dob1     The dob 1
+     * @param      marks11  The marks 11
+     * @param      marks22  The marks 22
+     * @param      marks33  The marks 33
+     * @param      total1   The total 1
+     * @param      cat1     The cat 1
+     */
 
     Student(final String name1, final String dob1, final int marks11,
         final int marks22, final int marks33, final int total1,
@@ -19,38 +55,100 @@ class Student implements Comparable<Student> {
         this.cat = cat1;
     }
 
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
     public String toString() {
         return name + "," + total +"," + cat;
     }
 
+    /**
+     * Getter function to get name.
+     * Complexity is 1.
+     *
+     * @return     { description_of_the_return_value }
+     */
     public String getname() {
         return name;
     }
 
+    /**
+     * Getting date of birth.
+     * Complexity is 1.
+     *
+     * @return     { description_of_the_return_value }
+     */
     public String getdob() {
         return dob;
     }
 
+    /**
+     * Getter function to get marks1.
+     * Complexity is 1.
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int getmarks1() {
         return marks1;
     }
-
+    /**
+     * Getter function to get marks2.
+     * Complexity is 1.
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int getmarks2() {
         return marks2;
     }
 
+    /**
+     * Getter function to get marks 3.
+     * Complexity is 1.
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int getmarks3() {
         return marks3;
     }
 
+    /**
+     * Getter function to get the total.
+     * Complexity is 1.
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int gettotal() {
         return total;
     }
+
+    /**
+     * Getter function to get the category.
+     * Complexity is.
+     *
+     * @return     { description_of_the_return_value }
+     */
 
     public String getcat() {
         return cat;
     }
 
+    /**
+     * Compare to function compares the total
+     * first then if they are equal the marks3 are
+     * compared and later marks 2 are compared. If
+     * they are also equal then the person who is younger
+     * is gievn the first priority.
+     * 
+     * Date is a string in the constructor that I have taken
+     * I have converted it it into String and then it is compared.
+     * Accordingly.b
+     *
+     * @param      that  The that
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int compareTo(Student that) {
         if (this.total > that.total) {
             return 1;
