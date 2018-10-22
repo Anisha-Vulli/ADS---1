f4424682927cacfa87b56191d61a599804959fa2
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-10-22 20:06:05 +0530

    "ads1_m19_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS - 1 assignments/M 19/19.1 BST/Solution.java
+++ ADS - 1 assignments/M 19/19.1 BST/Solution.java
@@ -1,4 +1,10 @@
+/**
+ * Scanner import.
+ */
 import java.util.Scanner;
+/**
+ * Class for book.
+ */
 class Book implements Comparable<Book> {
     /**
      * Name string.
@@ -115,7 +121,11 @@ class Node {
     public String getValue() {
         return value;
     }
-
+    /**
+     * Gets the count.
+     *
+     * @return     The count.
+     */
     public int getCount() {
         return count;
     }
@@ -448,18 +458,19 @@ class BinarySearchTree {
      * @return     { description_of_the_return_value }
      */
     private Node select(final Node x, final int k) {
-        if (x == null) return null;
-        //System.out.println(x.toString());
+        if (x == null) {
+            return null;
+        }
         int t = count(x.getLeft());
-        //System.out.println(t);
         if (t > k) {
             return select(x.getLeft(),  k);
         } 
         if (t < k) {
             return select(x.getRight(), k - t - 1); 
         }
-        if(t==k)
+        if (t == k) {
             return x;
+        }
         return x;
     }    
 }
@@ -516,3 +527,4 @@ final class Solution {
         }
     }
 }
+
